43ddc81be492661d4044722ed003d8238e8f6348
Stewart Young <stewart@Stewarts-MacBook-Pro.local>
2019-09-27 07:27:50 +0100

    Fix indentation

--- Algorithms/SymmetricTree.py
+++ Algorithms/SymmetricTree.py
@@ -20,24 +20,24 @@ class RecursiveSolution:
 
 class IterativeSolution:
     def isSymmetric(self, root):
-    if root is None:
-      return True
+        if root is None:
+            return True
 
-    stack = [[root.left, root.right]]
+        stack = [[root.left, root.right]]
 
-    while len(stack) > 0:
-      pair = stack.pop(0)
-      left = pair[0]
-      right = pair[1]
+        while len(stack) > 0:
+            pair = stack.pop(0)
+            left = pair[0]
+            right = pair[1]
 
-      if left is None and right is None:
-        continue
-      if left is None or right is None:
-        return False
-      if left.val == right.val:
-        stack.insert(0, [left.left, right.right])
+            if left is None and right is None:
+                continue
+            if left is None or right is None:
+                return False
+            if left.val == right.val:
+                stack.insert(0, [left.left, right.right])
 
-        stack.insert(0, [left.right, right.left])
-      else:
-        return False
-    return True
+                stack.insert(0, [left.right, right.left])
+            else:
+                return False
+        return True
