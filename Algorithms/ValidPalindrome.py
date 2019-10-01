c503c7aeac9bbd980d43a3df6f400a64f7d01ec0
Stewart Young <stewartyoung97@gmail.com>
2019-10-01 07:56:30 +0100

    Added valid palindrome

--- Algorithms/ValidPalindrome.py
+++ Algorithms/ValidPalindrome.py
@@ -1,3 +1,17 @@
+string = f'A man, a plan, a canal: Panama'
+
 class ValidPalindrome:
     def isPalindrome(self, string):
+        # remove non alphanumeric characters from string
+        for char in string:
+            if char.isalnum() == False:
+                string = string.replace(char, '')
         
+        # switch string to all lower case
+        string = string.lower()
+
+        # does the string equal the reversed string?
+        return string == string[::-1]
+
+test = ValidPalindrome()
+print(test.isPalindrome(string))
