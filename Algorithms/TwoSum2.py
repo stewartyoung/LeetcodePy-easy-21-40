cfba51817ca9c5473fc97f09361905c9c914e178
Stewart Young <stewartyoung97@gmail.com>
2019-10-04 08:32:33 +0100

    working twoSum2 after errors on previous version

--- Algorithms/TwoSum2.py
+++ Algorithms/TwoSum2.py
@@ -1,14 +1,23 @@
 egNumbers = [2,7,11,15]
 egTarget = 9
 
+egNumbers2 = [2,3,4]
+egTarget2 = 6
+
 class Solution:
     def twoSum(self, numbers, target):
-        for num in range(len(numbers)):
-            index1 = num
-            for i in range(index1 + 1,len(numbers)-1):
-                if numbers[index1] + numbers[i] == target:
-                    index2 = i
-                    return [index1 + 1, index2 + 1]
-
+        l = 0
+        r = len(numbers) - 1
+        while l <r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else: 
+                r -= 1
+                
 test = Solution()
 print(test.twoSum(egNumbers,egTarget))
+
+test2 = Solution()
+print(test2.twoSum(egNumbers2, egTarget2))
