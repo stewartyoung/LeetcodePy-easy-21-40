f456031442522508aad17c0fe3f434f8e8e1b454
Stewart Young <stewartyoung97@gmail.com>
2019-10-15 07:54:18 +0100

    Add factorial trailing zero "only working locally"

--- Algorithms/FactorialTrailingZero.py
+++ Algorithms/FactorialTrailingZero.py
@@ -4,4 +4,25 @@ def RecursiveFactorial(num):
     else:
         return num * RecursiveFactorial(num - 1)
 
-print(RecursiveFactorial(5))
+def IterativeFactorial(num):
+    factorial = 1
+    for i in range(1,num + 1):
+        factorial = factorial * i
+    return factorial
+
+class Solution:
+    def TrailingZero(self, num):
+        zeroCount = 0
+        if num == 0:
+            return zeroCount
+        testNumber = str(IterativeFactorial(num))
+        i = 1
+        while testNumber[-i] == '0':
+            zeroCount = zeroCount + 1
+            i = i + 1
+        return zeroCount
+
+print("example answer of the recursive factorial function for 7 is {}".format(RecursiveFactorial(7)))
+
+test = Solution()
+print(test.TrailingZero(9057))
